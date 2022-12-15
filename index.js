1341936a917f46c19c0c2275f6070647d34176aa
Chris Choute <christopherchoute@gmail.com>
2022-12-15 17:48:19 -0500

    commit tree a1ae8f57383bbcd5f45f1a4a3a503e1417ecf08c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let name = names[i];
+      let message = `Thank you, ${name}, for the wonderful ${event} gift!`
+      messages = [...messages, message];
+    }
+  
+    return messages;
+}
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
